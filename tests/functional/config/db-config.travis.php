7d59f900672636fac9d5fced88a66a3c87804baa
Oleg Kulik <olegkulik1985@gmail.com>
2018-02-28 19:04:54 +0200

    adding infrastructure;

---
+++ tests/functional/config/db-config.travis.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @docs at http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html
+ */
+return [
+    'url' => 'mysql://root@127.0.0.1:3306/dataflow?charset=UTF8',
+];
